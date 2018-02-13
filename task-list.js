2ab6edfa5849cb1fb306b0473ea9a74ef34cc413
Bryan Gonzalez <bryanisimo@gmail.com>
2018-02-12 20:25:08 -0600

    Ya borra

--- task-list.js
+++ task-list.js
@@ -11,6 +11,15 @@ $(document).ready(function(){
   var $categoria = $('select#categoria');
   var template = '<li class="list-group-item list-group-item-action"></li>';
 
+  $('body').delegate('button.boton-eliminar','click',function( event ){
+    event.preventDefault();
+    if(confirm('¿Está usted completamente segura?')){
+      var $target = $(this).parent();
+      $target.slideUp(500,function(){
+        $target.remove();
+      });
+    }
+  });
   $formulario_de_lista.on('submit', function( event ){
     event.preventDefault();
     var texto_de_producto = $producto.val().trim();
@@ -21,8 +30,10 @@ $(document).ready(function(){
       var $new_row = $(
         '<li class="list-group-item bg-'+clase.value+' list-group-item-action">' +
         texto_de_producto +
+        '<button class="btn btn-danger btn-sm float-right boton-eliminar"><i class="fas fa-times"></i></button>' +
         '</li>'
       );
+
       $lista_principal.append( $new_row );
 
       $new_row.hide().fadeIn();
